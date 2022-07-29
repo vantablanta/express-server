27da303032aa11778b6fcf39b337eb5dfb0b6be9
vanta blanta <vantablanta@gmail.com>
2022-07-29 18:52:20 +0300

    dynamic routing in express using regex

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,21 @@ router.get('/', (req, res) => {
 })
 
 
-router.get('/details', (req, res) => {
-    res.send("GET Request For User Details")
+router.get('/details/:id', (req, res) => {
+    res.send("GET Request For User Details. User" + req.params.id)
+})
+
+router.get('/search/:state/:city', (req, res) => {
+    res.send("GET Request For User Details. Search By " + req.params.state + req.params.city)
+})
+
+
+router.get('/number-regex/search/:key([0-9]{4})', (req, res) => {
+    res.send("GET Request For User Details. Search By " + req.params.key)
+})
+
+router.get('/string-regex/search/:key([a-zA-Z]{4})', (req, res) => {
+    res.send("GET Request For User Details. Search By " + req.params.key)
 })
 
 
